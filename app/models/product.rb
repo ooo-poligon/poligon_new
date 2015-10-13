a6dff4bf687829310344680634d9a2e365fc8821
Igor Klekotnev <klekotnev@gmail.com>
2015-10-13 13:22:56 +0300

    'Validation!'

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,10 @@
 # -*- encoding : utf-8 -*-
 class Product < ActiveRecord::Base
-  attr_accessible :image_url, :retail_price, :short_description, :title
+  validates :image_url, :short_description, :title, presence: true
+  validates :retail_price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :title, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+      with: %r{\.(gif|jpg|png)$}i,
+      message: 'URL должен указывать на изображение формата GIF, JPG или PNG.'
+  }
 end
