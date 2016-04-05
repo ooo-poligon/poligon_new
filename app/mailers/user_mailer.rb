cf55927d435b2a7a2f1ebfd37c7abae827bdd1bf
Igor Klekotnev <klekotnev@gmail.com>
2016-04-05 17:35:54 +0300

    Work with emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
-class UserMailer < ApplicationMailer
-  default from: 'webmaster-poligon@yandex.ru'
+class UserMailer < ActionMailer::Base
+  default from: 'mailer@poligon.info', to: { User.pluck(:email) }
 
   def welcome_email(user)
     @user = user
-    @url  = 'http://example.com/login'
-    mail(to: @user.email, subject: 'Подписка на новости сайта new.poligon.info')
+    @url  = 'http://new.poligon.info/feedback/confirm_subscription'
+    mail(from: 'webmaster-poligon@yandex.ru', to: @user.email, subject: 'Подписка на новости сайта new.poligon.info')
   end
 
 end
