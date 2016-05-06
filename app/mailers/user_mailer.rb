d0c41146f83c8e84fc65b835225372987668379c
Igor Klekotnev <klekotnev@gmail.com>
2016-05-06 12:51:57 +0300

    unified mail receiver

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,31 +3,33 @@ class UserMailer < ActionMailer::Base
   def welcome_email(email)
     @email = email
     @url  = 'http://new.poligon.info'
-    mail(to: @email, subject: 'Hello!!!')
+    mail(to: @email, subject: 'Подписка на новости сайта new.poliogn.info')
   end
 
   def goodbye_email(email)
     @email = email
     @url  = 'http://new.poligon.info'
-    mail(to: @email, subject: 'Goodbye!')
+    mail(to: @email, subject: 'Отказ от подписки на новости сайта new.poliogn.info')
   end
 
   def catalogs_order_email(name, email, company, address, catalogue)
+    @receiver  = Setting.find_by(title: 'siteOrdersReceiver').text_value
     @name      = name
     @email     = email
     @company   = company
     @address   = address
     @catalogue = catalogue
-    mail(to: 'webmaster@poligon.info', subject: 'Заказ каталогов с сайта new.poliogn.info')
+    mail(to: @receiver, bcc: 'webmaster@poligon.info', subject: 'Заказ каталогов с сайта new.poliogn.info')
   end
 
   def farnell_order_email(name, email, company, phone, order)
-    @name    = name
-    @email   = email
-    @company = company
-    @phone   = phone
-    @order   = order
-    mail(to: 'webmaster@poligon.info', subject: 'Заказ FARNELL с сайта new.poliogn.info')
+    @receiver = Setting.find_by(title: 'siteOrdersReceiver').text_value
+    @name     = name
+    @email    = email
+    @company  = company
+    @phone    = phone
+    @order    = order
+    mail(to: @receiver, bcc: 'webmaster@poligon.info', subject: 'Заказ FARNELL с сайта new.poliogn.info')
   end
 
 end
