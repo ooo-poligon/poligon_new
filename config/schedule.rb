d723906f95205a499ef4c35406ee954959bb548c
Igor Klekotnev <klekotnev@gmail.com>
2016-06-14 13:34:27 +0300

    change time to send mail about certificates

--- config/schedule.rb
+++ config/schedule.rb
@@ -22,7 +22,7 @@
 set :environment, "production"
 set :output, {error: "log/cron_error_log.log", standard: "log/cron_log.log"}
 
-every 1.day, at: '13:00 pm' do
+every 1.day, at: '13:35 pm' do
   # specify the task name as a string
   rake 'check_certificates_expiration'
 end
