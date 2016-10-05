bcd67e657d30408005a388916b0c95794e9599dc
Igor Klekotnev <klekotnev@gmail.com>
2016-10-05 11:39:28 +0300

    rearrange pdf list

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,9 @@
 module CategoriesHelper
+  # require 'rmagick'
+  # #too slow for real application...(
+  # def pdf_preview(pdf_path)
+  #   pdf = Magick::ImageList.new(pdf_path + "[0]")
+  #   scaled_page = pdf.scale(300, 450)
+  #   scaled_page.write("tmp/pdf_preview.jpg")
+  # end
 end
