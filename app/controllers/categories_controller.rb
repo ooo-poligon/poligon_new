479cef1101f5c3a11583bbe507807e27d78b48d2
Igor Klekotnev <klekotnev@gmail.com>
2018-05-09 15:14:13 +0300

    categories products list logic and more

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,4 @@
 class CategoriesController < ApplicationController
-
   def index
     @upLevelCategories = Category.available.where(parent: 1)
     @addCBR = Setting.find_by title: 'addCBR'
@@ -27,23 +26,33 @@ class CategoriesController < ApplicationController
     @parents_array.each do |parent|
       @parents_categories_ids.push parent.id
     end
+
+    recentAndSubCategoriesIds = []
+    recentAndSubCategoriesIds.push @currentCategoryId
+    @subCategories.each do |sc|
+      recentAndSubCategoriesIds.push sc.id
+    end
+
     @vendors_categories_ids = [142, 5094, 74, 5414, 5535, 5818, 5933, 5583, 5512, 6441, 4847, 6321, 5650]
     @products = []
     addCBR = Setting.find_by title: 'addCBR'
-    Product.available.where(category_id: @currentCategoryId).each do |product|
-      vendor = Vendor.find(product.vendor_id)
-      one_product_array = []
-      product_price = 0
-      if product.currency_id == 1
-        product_price = (product.price * (@courseEuro + (@courseEuro / 100) * addCBR.text_value.to_f) * product.rate).round(2)
-      elsif product.currency_id == 2
-        product_price = product.rub_retail.round(2)
+
+    recentAndSubCategoriesIds.each do |categoryId|
+      Product.available.where(category_id: categoryId).each do |product|
+        vendor = Vendor.find(product.vendor_id)
+        one_product_array = []
+        product_price = 0
+        if product.currency_id == 1
+          product_price = (product.price * (@courseEuro + (@courseEuro / 100) * addCBR.text_value.to_f) * product.rate).round(2)
+        elsif product.currency_id == 2
+          product_price = product.rub_retail.round(2)
+        end
+        quantity = !Quantity.find_by(product_id: product.id).nil? && Quantity.find_by(product_id: product.id).stock > 0 ? Quantity.find_by(product_id: product.id).stock : 0
+        if quantity > 0 || (quantity == 0 && recentAndSubCategoriesIds.length == 1)
+          one_product_array.push(product, product_price, quantity, vendor)
+          @products.push(one_product_array)
+        end
       end
-      quantity = !Quantity.find_by(product_id: product.id).nil? && Quantity.find_by(product_id: product.id).stock > 0 ? Quantity.find_by(product_id: product.id).stock : 0
-      one_product_array.push(product, product_price, quantity, vendor)
-      @products.push(one_product_array)
     end
-
   end
-
 end
