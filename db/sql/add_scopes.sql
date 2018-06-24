3aeb97f7e24e4351304448c3306ce094d20ba931
Igor Klekotnev <klekotnev@gmail.com>
2018-06-24 16:41:49 +0300

    hotfix for scope.name is nil

---
+++ db/sql/add_scopes.sql
@@ -0,0 +1,6 @@
+INSERT INTO `poligon`.`scopes` (`name`) VALUES ('Насосы и двигатели');
+INSERT INTO `poligon`.`scopes` (`name`) VALUES ('Альтернативная энергетика');
+INSERT INTO `poligon`.`scopes` (`name`) VALUES ('Энергосбережение и жкх');
+INSERT INTO `poligon`.`scopes` (`name`) VALUES ('Производство');
+INSERT INTO `poligon`.`scopes` (`name`) VALUES ('Мобильность');
+INSERT INTO `poligon`.`scopes` (`name`) VALUES ('Прочие примеры');
