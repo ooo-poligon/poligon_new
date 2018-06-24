d83d94017e22f487196355eea93a2c17666bbf49
Igor Klekotnev <klekotnev@gmail.com>
2018-06-24 16:20:24 +0300

    change migration for examples_images

--- db/migrate/20180624085657_add_example_images_to_example.rb
+++ db/migrate/20180624085657_add_example_images_to_example.rb
@@ -1,5 +1,5 @@
 class AddExampleImagesToExample < ActiveRecord::Migration
   def change
-    add_column :examples, :example_images, :json
+    add_column :examples, :example_images, :string
   end
 end
