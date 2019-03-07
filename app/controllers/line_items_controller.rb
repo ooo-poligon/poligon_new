514d93695c9cd524fc9fe5c24ae7f4472c3b9093
Laptev Vlad <laptev.vlad@gmail.com>
2019-03-07 11:37:06 +0200

    Changed forse_ssl to true

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -37,6 +37,8 @@ class LineItemsController < ApplicationController
     overflow = quantity - stock
     @line_item = @cart.add_product(product.id, quantity, price, overflow)
 
+
+    binding.pry
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to @line_item.cart }
